a7784f4f90cec60fa6a2628ba8abcd7959640fe7
Niko Arellano <nikoarellano@Nikos-Air.hitronhub.home>
2017-07-15 11:51:02 -0700

    added label to navbar

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,8 +3,9 @@
  */
 
 import React from 'react';
-import { FontAwesome } from '@expo/vector-icons';
-import { TabNavigator, TabBarBottom } from 'react-navigation';
+import {View, Text} from 'react-native';
+import {FontAwesome} from '@expo/vector-icons';
+import {TabNavigator, TabBarBottom} from 'react-navigation';
 
 import Colors from '../constants/Colors';
 
@@ -14,53 +15,77 @@ import AddListingScreen from '../screens/AddListingScreen';
 import ProductScreen from '../screens/ProductScreen'
 
 export default TabNavigator(
-  {
+    {
 
-    Home: {
-      screen: HomeScreen,
-    },
-    Add: {
-      screen: AddListingScreen,
-    },
-
-    Browse: {
-      screen: BrowseScreen,
-    }
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      // Set the tab bar icon
-      tabBarIcon: ({ focused }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        switch (routeName) {
-          case 'Home':
-            iconName = 'home';
-            break;
-          case 'Browse':
-            iconName = 'search';
-            break;
-          case 'Add':
-            iconName = 'plus-circle';
-            break;
+        Home: {
+            screen: HomeScreen,
+        },
+        Add: {
+            screen: AddListingScreen,
+        },
+        Browse: {
+            screen: BrowseScreen,
+        },
+        Profile: {
+            screen: BrowseScreen,
         }
-        return (
-          <FontAwesome
-            name={iconName}
-            size={32}
-            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-          />
-        );
-      },
-    }),
-    // Put tab bar on bottom of screen on both platforms
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    // Disable animation so that iOS/Android have same behaviors
-    animationEnabled: false,
-    // Don't show the labels
-    tabBarOptions: {
-      showLabel: false,
     },
-  }
+    {
+        navigationOptions: ({navigation}) => ({
+            // Set the tab bar icon
+            tabBarIcon: ({focused}) => {
+                const {routeName} = navigation.state;
+                let iconName, screenName;
+                switch (routeName) {
+                    case 'Home':
+                        iconName = 'map-marker';
+                        screenName = 'Near Me';
+                        break;
+                    case 'Browse':
+                        iconName = 'navicon';
+                        screenName = routeName;
+                        break;
+                    case 'Add':
+                        iconName = 'edit';
+                        screenName = 'Post';
+                        break;
+                    case 'Profile':
+                        iconName = 'user';
+                        screenName = routeName;
+                        break;
+
+                }
+                return (
+                    <FontAwesome
+                        name={iconName}
+                        size={32}
+                        color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+                    />
+                );
+            },
+            tabBarLabel : ({focused}) => {
+                const {routeName} = navigation.state;
+                let screenName;
+                switch (routeName) {
+                    case 'Home':
+                        return 'Near Me';
+                    case 'Browse':
+                        return routeName;
+                    case 'Add':
+                        return 'Post';
+                    case 'Profile':
+                        return routeName;
+                }
+            }
+        }),
+        // Put tab bar on bottom of screen on both platforms
+        tabBarComponent: TabBarBottom,
+        tabBarPosition: 'bottom',
+        // Disable animation so that iOS/Android have same behaviors
+        animationEnabled: false,
+        // Don't show the labels
+        tabBarOptions: {
+            showLabel: true,
+        }
+    }
 );
